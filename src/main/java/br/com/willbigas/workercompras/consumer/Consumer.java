35482ead1c7e16ed494862fa2c57425999140bc6
William Bigas Mauro <williambmauro@hotmail.com>
2022-12-03 20:53:13 -0300

    Adicionado sistema de notificação por e-mail

--- src/main/java/br/com/willbigas/workercompras/consumer/Consumer.java
+++ src/main/java/br/com/willbigas/workercompras/consumer/Consumer.java
@@ -1,8 +1,10 @@
 package br.com.willbigas.workercompras.consumer;
 
 import br.com.willbigas.workercompras.model.Pedido;
+import br.com.willbigas.workercompras.service.EmailService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +13,18 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class Consumer {
 
 	private final ObjectMapper mapper;
+	private final EmailService emailService;
 
 	@RabbitListener(queues = {"${queue_name}"})
 	public void consumer(@Payload Message message) throws IOException {
 		Pedido pedido = mapper.readValue(message.getBody(), Pedido.class);
 		System.out.println("Mensagem recebida - Worker Compras -> "  + pedido);
+		emailService.notificarCliente("williambmauro@hotmail.com");
 	}
 }
